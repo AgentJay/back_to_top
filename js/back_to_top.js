2f336f0cf198767f6dd001a4a1a1a180f676ac0f
shkiper <shkiper@2261152.no-reply.drupal.org>
2014-11-12 05:01:49 +0100

    Issue #2203165 by andrii zahura, acke: Port Back To Top to Drupal 8.

--- js/back_to_top.js
+++ js/back_to_top.js
@@ -4,11 +4,11 @@
 			var exist= jQuery('#backtotop').length;
       if(exist == 0) {
         $("body", context).once(function() {
-          $(this).append("<div id='backtotop'>"+Drupal.t(Drupal.settings.back_to_top.back_to_top_button_text)+"</div>");
+          $(this).append("<div id='backtotop'>"+Drupal.t(drupalSettings.back_to_top.back_to_top_button_text)+"</div>");
         });
       }
 			$(window).scroll(function() {
-				if($(this).scrollTop() > Drupal.settings.back_to_top.back_to_top_button_trigger) {
+				if($(this).scrollTop() > drupalSettings.back_to_top.back_to_top_button_trigger) {
 					$('#backtotop').fadeIn();	
 				} else {
 					$('#backtotop').fadeOut();
