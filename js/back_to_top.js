ecc88ed7842ed77f9c8663e6f224e5a46bbbe053
sumitmadan <sumitmadan@1538790.no-reply.drupal.org>
2016-08-19 14:44:51 +0530

    Issue #2638332 by sumitmadan, Dom.: Update 8.x-1.x with Drupal 8 Latest Release

--- js/back_to_top.js
+++ js/back_to_top.js
@@ -1,31 +1,31 @@
 (function ($) {
-	Drupal.behaviors.backtotop = {
-		attach: function(context) {
-			var exist= jQuery('#backtotop').length;
-      if(exist == 0) {
-        $("body", context).once(function() {
-          $(this).append("<div id='backtotop'>"+Drupal.t(drupalSettings.back_to_top.back_to_top_button_text)+"</div>");
-        });
-      }
-			$(window).scroll(function() {
-				if($(this).scrollTop() > drupalSettings.back_to_top.back_to_top_button_trigger) {
-					$('#backtotop').fadeIn();	
-				} else {
-					$('#backtotop').fadeOut();
-				}
-			});
+    Drupal.behaviors.backtotop = {
+        attach: function (context, settings) {
+            var exist = $('#backtotop').length;
+            if (exist == 0) {
+                $("body", context).once('backtotop').each(function () {
+                    $('body').append("<div id='backtotop'>" + Drupal.t(settings.back_to_top.back_to_top_button_text) + "</div>");
+                });
+            }
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > settings.back_to_top.back_to_top_button_trigger) {
+                    $('#backtotop').fadeIn();
+                } else {
+                    $('#backtotop').fadeOut();
+                }
+            });
 
-      $('#backtotop', context).once(function() {
-			  $(this).click(function() {
-			    $("html, body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function() {
-            $('html, body').stop();
-          });
-          $('html,body').animate({ scrollTop: 0 }, 1200, 'easeOutQuart', function() {
-            $("html, body").unbind("scroll mousedown DOMMouseScroll mousewheel keyup");
-          });
-          return false;
-			  });
-			});
-		}
-	};
+            $('#backtotop', context).once('backtotop').each(function () {
+                $(this).click(function () {
+                    $("html, body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function () {
+                        $('html, body').stop();
+                    });
+                    $('html,body').animate({scrollTop: 0}, 1200, 'easeOutQuart', function () {
+                        $("html, body").unbind("scroll mousedown DOMMouseScroll mousewheel keyup");
+                    });
+                    return false;
+                });
+            });
+        }
+    };
 })(jQuery);
